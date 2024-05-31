a82614d75cc1b463fc9f65cdb43e85c5f0f29900
StefanoMela <stefano.mela25@gmail.com>
2024-05-31 11:22:41 +0200

    feat MS 1-2

--- models/event.js
+++ models/event.js
@@ -1,5 +1,6 @@
-const fs = require('fs');
 const eventsDB = require('../data/events.json')
+const path = require("path");
+const fs = require('fs');
 
 class Event {
     constructor(id, title, description, date, maxSeats) {
@@ -9,11 +10,21 @@ class Event {
         this.date = date;
         this.maxSeats = maxSeats;
     }
-    static saveEvent(newEvent, eventsDB) {
-        
+    static saveEvent(fileName, newEvent) {
+        const filePath = path.join(__dirname, '..' + 'data' + fileName + ".json");
+        const dataToJson = JSON.stringify(newEvent)
+        fs.writeFileSync(filePath, dataToJson);
     }
 
     static readEvent(event) {
-        
+        const filePath = path.join(__dirname, '..' + 'data' + fileName + ".json");
+        const dataToRead = fs.readFileSync(filePath, "utf-8");
+        return JSON.parse(dataToRead)
     }
 }
+
+const evento1 = new Event(1, 'Il mio primo evento', 'Una bellissima festa', 'stasera', 234);
+
+Event.saveEvent(eventsDB, evento1);
+
+Event.readEvent(evento1);
